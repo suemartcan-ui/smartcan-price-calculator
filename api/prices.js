a224914d2310dbc14f7437fbef1e4064ed6a9e58
suemartcan-ui <suemartcan@gmail.com>
2026-06-25 11:40:54 -0300

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -2,9 +2,12 @@ export default async function handler(req, res) {
   const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQLX4ElvfcdFRFotB1ZYYs-YI-KdS4CqgTLSbzD84XUeBuqGqApjetOaA6oqpf41WCrERbMnhWY7mYx/pub?gid=393936923&single=true&output=csv";
   
   const response = await fetch(CSV_URL);
-  const text = await response.text();
+  const buffer = await response.arrayBuffer();
+  const text = new TextDecoder("utf-8").decode(buffer);
   
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Content-Type", "text/csv");
+  res.setHeader("Content-Type", "text/csv; charset=utf-8");
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+  res.setHeader("Pragma", "no-cache");
   res.status(200).send(text);
 }
